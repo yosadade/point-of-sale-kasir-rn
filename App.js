f82d746d8e2c32bc59ae6ee3c1c2d298905fe585
Yosada Dede <masyosad@gmail.com>
2024-06-22 10:06:19 +0000

    first commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Text>App</Text>
+    </NavigationContainer>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
---
+++ src/screens/index.js
@@ -0,0 +1,6 @@
+import Dashboard from './Dashboard';
+import Report from './Report';
+import Setting from './Setting';
+import Splash from './Splash';
+
+export {Dashboard, Report, Setting, Splash};
